ab29c229e264e91b025e0c4a7be13895b7fd1b2a
Gromy4 <mr.gromy4@gmail.com>
2021-04-14 23:52:07 +0300

    Commit(zapis v slovar)

--- Site/myform.py
+++ Site/myform.py
@@ -1,18 +1,23 @@
 from bottle import post, request
 import re
+import pdb
 
 @post('/home', method='post')
 def my_form():
 
+    questions = {}
+
     mail = request.forms.get('ADRESS')
     question = request.forms.get('QUEST')
-
-    reg = '^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]+\w{2,3}$'
+    reg = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-.]+[^.]\.[a-zA-Z0-9]{2,3}$"
+   
    
     if (len(mail) == 0) or (len(question) == 0):
         return "Form is empty! Fill the form."
     else:
         if (re.search(reg, mail)):
+            questions[mail] = question
+            pdb.set_trace()
             return "Thanks! The answer will be sent to the mail %s" % mail
         else:
             return "Mail doesn`t match to form"
